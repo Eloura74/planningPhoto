eacc6d76b011e9cfbca5aa29866663777e17fadc
Quentin <faber.quentin@gmail.com>
2026-04-25 18:12:50 +0200

    Add VITE_API_URL in vite.config.js for production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,27 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true
-      }
-    }
+export default defineConfig(({ mode }) => {
+  const config = {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: "http://localhost:5000",
+          changeOrigin: true,
+        },
+      },
+    },
+  };
+
+  if (mode === "production") {
+    config.define = {
+      "import.meta.env.VITE_API_URL": JSON.stringify(
+        "https://planningphoto.onrender.com/api",
+      ),
+    };
   }
-})
+
+  return config;
+});
